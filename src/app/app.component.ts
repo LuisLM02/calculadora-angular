3b87bc269502ccabb5b70b6920fb620ca73db0ee
LuisLM02 <luis.lopez.martell.llm@outlook.com>
2022-06-28 11:08:37 -0500

    calculadora un componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+
+  operacion:string = "";
+
+  Sumar0(): void {
+    this.operacion += 0;
+  }
+  Sumar1(): void {
+    this.operacion += 1;
+  }
+  Sumar2(): void {
+    this.operacion += 2;
+  }
+  Sumar3(): void {
+    this.operacion += 3;
+  }
+  Sumar4(): void {
+    this.operacion += 4;
+  }
+  Sumar5(): void {
+    this.operacion += 5;
+  }
+  Sumar6(): void {
+    this.operacion += 6;
+  }
+  Sumar7(): void {
+    this.operacion += 7;
+  }
+  Sumar8(): void {
+    this.operacion += 8;
+  }
+  Sumar9(): void {
+    this.operacion += 9;
+  }
+  Suma(): void {
+    if(this.operacion.substr(-1) === '+') return;
+    this.operacion += '+';
+  }
+  Resta(): void {
+    if(this.operacion.substr(-1) === '-') return;
+    this.operacion += '-';
+  }
+  Delete(): void {
+    this.operacion = this.operacion.substring(0, this.operacion.length - 1)
+  }
+  Clear(): void {
+    this.operacion = '';
+  }
+  SumarAll(): void {
+    this.operacion = eval(this.operacion).toString();
+  }
+
 }
